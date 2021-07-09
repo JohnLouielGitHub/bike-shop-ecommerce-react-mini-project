eaa23150c4fa601c1f1fd3e88b1487f73b1efb7c
CodeLou <tamargo.louiel@gmail.com>
2021-07-09 15:39:54 +0800

    initial build-1.1

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,4 +1,4 @@
-import Image from "../../node_modules/next/image";
+import ImageHero from "../../node_modules/next/image";
 import Picture from "../../public/hero-1.png";
 import Picture2 from "../../public/hero-2.png";
 import Picture3 from "../../public/hero-3.png";
